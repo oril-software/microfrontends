f7ded5e6eda61471cb4dbf671f7e68bcde543d7a
Ivan Prots <ivan.prots@oril.co>
2023-07-18 14:51:35 +0300

    first commit

---
+++ login-app/src/components/ColorSchemeToggle.tsx
@@ -0,0 +1,37 @@
+import React, { memo } from "react";
+import { IconButtonProps, useColorScheme, IconButton } from "@mui/joy";
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+
+const ColorSchemeToggle = ({ onClick, ...props }: IconButtonProps) => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="plain" color="neutral" disabled />;
+  }
+  return (
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="plain"
+      color="neutral"
+      aria-label="toggle light/dark mode"
+      {...props}
+      onClick={(event) => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+        onClick?.(event);
+      }}
+    >
+      {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
+  );
+}
+
+export default memo(ColorSchemeToggle);
